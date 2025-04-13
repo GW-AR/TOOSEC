96b4e80663442fbaddea206c35306a737efd4bf6
GrzegorzWawruch <wawruch.grzegorz@gmail.com>
2025-04-13 14:36:02 +0200

    basics things works

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
+
 from flask import Flask
 from flask import render_template
+from flask import request
+import url_class
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/url', methods=['GET', 'POST'])
+def url():
+
+    if request.method == 'GET':
+        page = url_class.Url("")
+        return render_template('url.html', page_object=page)
+
+    elif request.method == 'POST':
+
+        page = url_class.Url(request.form['link_url'])
+        page.check_url_address()
+        return render_template('url.html', page_object=page)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ url_class.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_URL = "https://www.virustotal.com/api/v3/urls"
+
+headers = {
+    "accept": "application/json",
+    "x-apikey": os.environ['API_KEY'],
+    "content-type": "application/x-www-form-urlencoded"
+}
+
+
+class Url:
+    def __init__(self, url):
+        self.address_url = url
+        self.results_list = []
+        self.list_of_security_tools = []
+
+    def check_url_address(self):
+
+        payload = {"url": self.address_url }
+
+        response = requests.post(url=API_URL, data=payload, headers=headers)
+        response_json = response.json()
+
+        url_get = API_URL + "/" + response_json['data']['id'].split('-')[1]
+
+        response_get = requests.get(url_get, headers=headers)
+        response_get = response_get.json()
+
+        self.list_of_security_tools = [ elm for elm in response_get['data']['attributes']['last_analysis_results']]
+
+        self.results_list = { elm : {
+            "method" : response_get['data']['attributes']['last_analysis_results'][elm]['method'],
+            "category" : response_get['data']['attributes']['last_analysis_results'][elm]['category'],
+            "result" : response_get['data']['attributes']['last_analysis_results'][elm]["result"]
+        } for elm in response_get['data']['attributes']['last_analysis_results']}
