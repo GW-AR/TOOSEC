00d88c7b5570b6815b2e17673bdc639d1ebec1cc
alormada <adam.rola110@gmail.com>
2025-04-15 11:40:50 +0200

    getting data from the webpage (text to encrypt) and printing it out on the encrypted text placeholder

--- encryption.py
+++ encryption.py
@@ -5,3 +5,23 @@
 - Base64
 - etc
 """
+
+from bs4 import BeautifulSoup
+
+
+class Cipher():
+    def __init__(self):
+        self.text_to_encrypt = ""
+        self.encrypted_text = "hej"
+        
+
+class CaesarCipher(Cipher):
+    def __init__(self):
+        super().__init__()
+        self.shift = 0
+
+
+cezar = CaesarCipher()
+print(cezar.encrypted_text)
+
+
--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask # type: ignore
 from flask import render_template # type: ignore
+from flask import request
+import encryption
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    message = request.form.get("message")
+    cipher = request.form.get("cipher")
+    key = request.form.get("key")
+    return render_template('encrypt.html', message=message, cipher=cipher, key=key)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
